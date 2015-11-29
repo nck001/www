a1e82f0df393ff183c92d06d0004d587917aec34
nck001 <nisalchamara@gmail.com>
2015-11-29 21:28:13 +0530

    js

---
+++ jquery-mobile/custom.js
@@ -0,0 +1,69 @@
+// JavaScript Document
+
+function fetch_and_display_posts()
+{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://alzlanka.org/test/wp-admin/admin-ajax.php?action=posts&reusername=" + encodeURIComponent(username));
+    xhr.onload = function(){
+		alert(xhr.responseText);
+        var posts_array = JSON.parse(xhr.responseText);
+		//alert(posts_array);
+
+        var html = "";
+
+        for(var count = 0; count < posts_array.length; count++)
+        {
+            var title = posts_array[count][0];
+            var link = posts_array[count][1];
+            var date = posts_array[count][2];
+            var image = posts_array[count][3];
+
+            html = html + "<li>" + "<a href='javascript:open_browser(\"" + link + "\")'>" + "<img height='128' width='128' src='" + image + "'>" + "<h2>" + title + "</h2>" + "<p>" + date + "</p></a></li>";
+        }
+
+        document.getElementById("posts").innerHTML = html;
+        $("#posts").listview("refresh");
+    }
+    xhr.send();
+}
+
+var username = '';
+
+function login()
+{
+	
+    username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if(username == "")
+    {
+        alert("Please enter username", null, "Username Missing", "OK");
+        return;
+    }
+
+    if(password == "")
+    {
+        alert("Please enter password", null, "Password Missing", "OK");  
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://alzlanka.org/test/wp-admin/admin-ajax.php?action=login&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password));
+    xhr.onload = function(){
+        if(xhr.responseText == "FALSE")
+        {
+            alert("Wrong Username and Password", null, "Wrong Creds", "Try Again");
+        }
+        else if(xhr.responseText == "TRUE")
+        {
+            fetch_and_display_posts();
+            $("#page_two_link").click();
+        }
+    }   
+    xhr.send();
+}
+
+function open_browser(link)
+{
+    window.open(link, '_blank', 'location=yes');
+}
