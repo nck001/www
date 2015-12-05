4f3b9eb44e0d6d76acacc4e07b6e2c457c181ae4
nck001 <nisalchamara@gmail.com>
2015-12-06 01:49:07 +0530

    1

--- themes/custom.js
+++ themes/custom.js
@@ -13,45 +13,75 @@ $( document ).ready(function() {
         }
         else if(xhr.responseText == "TRUE")
         {
-            fetch_and_display_posts();
+            fetch_and_display_posts("OLD");
             $("#latest_link").click();
         }
     }   
     xhr.send();
 });
 
-function fetch_and_display_posts()
+var page=0;
+//var status = "NEXT";
+function fetch_and_display_posts(status)
 {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://alzlanka.org/test/wp-admin/admin-ajax.php?action=posts&username=" + encodeURIComponent(username) + "&password=" + encodeURIComponent(password));
+	//var chkEmpty ="EMPTY";
+	//var dateCnt =15;
+	if (status=="OLD"){
+	page = page+1;
+	}else
+	
+	if (status=="NEW"&&page>1){
+	page = page-1;
+	}else
+	
+	if (status=="LATEST"&&page>1){
+	page = 1;	
+	}
+	
+	
+	
+	var xhr = new XMLHttpRequest();
+	
+	//while(chkEmpty == "EMPTY"){
+	
+    
+    xhr.open("GET", "http://alzlanka.org/test/wp-admin/admin-ajax.php?action=posts&page=" + encodeURIComponent(page));
     xhr.onload = function(){
 		//alert(xhr.responseText);
-        var posts_array = JSON.parse(xhr.responseText);
+        
 		//alert(posts_array);
 
 		if(xhr.responseText == "FALSE")
         {
-            alert("Wrong Username and Password", null, "Wrong Creds", "Try Again");
-        } else {
+            $("#logout_link").click();
+			//chkEmpty ="FALSE";
+        }
+		else {
 
 
-
-        var html = '<ul data-role="listview" data-inset="true"><li data-role="list-divider">' + date +'<span class="ui-li-count">2</span>';
+		var posts_array = JSON.parse(xhr.responseText);
+        var html = '<ul data-role="listview" data-inset="true">';
 		
-
-        for(var count = 0; count < posts_array.length; count++)
+		var prevDate = '';
+        for(var count = 1; count < posts_array.length; count++)
         {
             var title = posts_array[count][0];
             var link = posts_array[count][1];
             var date = posts_array[count][2];
             //var image = posts_array[count][3];
+			
+			if(date!=prevDate){
+				 html = html + '<li data-role="list-divider">' + date +'<span class="ui-li-count">'+posts_array[0]+'</span>';
+				}
 
-            html = html + '<li><a href="#single"><p><b>'+title+'</b></p>'+
+            html = html + '<li><a href="#single" onClick="post_content(\''+link+'\');"><p><b>'+title+'</b></p>'+
 						 // '<p>categories</p>'+
 						  
                 		  '</a></li>'
+			prevDate = date;			  
 							
         }
+		//chkEmpty ="TRUE";
 		html = html +'</li></ul>'
         document.getElementById("latest-content").innerHTML = html;
 						var $list = $('#latest-content');
@@ -60,21 +90,60 @@ function fetch_and_display_posts()
 						} else {//this list needs to be initialized
 							$list.trigger('create');
 						}
-    }
+						
+						
+    }//else
 	
-	}
+	}//onload
 	
     xhr.send();
+	
+	
+	//}//while
 }
 
-var username = '';
-var password = '';
+
+function post_content(urllink)
+{
+	
+	var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://alzlanka.org/test/wp-admin/admin-ajax.php?action=single&link=" + encodeURIComponent(urllink));
+    xhr.onload = function(){
+		//alert(xhr.responseText);
+        
+		//alert(posts_array);
+
+		if(xhr.responseText == "FALSE")
+        {
+            $("#logout_link").click();
+			//chkEmpty ="FALSE";
+        }
+		else {
+
+
+		var post_array = JSON.parse(xhr.responseText);
+		var title = post_array[0];
+		var content = post_array[1];
+        var html = '<h3>'+title+'</h3><br>'+content;
+		document.getElementById("single-content").innerHTML = html;
+	
+						
+    }//else
+	
+	}//onload
+	
+    xhr.send();
+	
+	
+}
+
+
 
 function login()
 {
 	
-    username = document.getElementById("username").value;
-    password = document.getElementById("password").value;
+   var username = document.getElementById("username").value;
+   var password = document.getElementById("password").value;
 	var err = '';
     if(username == "")
     {
@@ -103,7 +172,7 @@ function login()
         }
         else if(xhr.responseText == "TRUE")
         {
-            fetch_and_display_posts();
+            fetch_and_display_posts("OLD");
             $("#latest_link").click();
         }
     }   
@@ -120,7 +189,7 @@ function logout()
 		//alert(xhr.responseText);
         if(xhr.responseText == "TRUE")
         {
-            fetch_and_display_posts();
+           
             $("#logout_link").click();
         }
     }   
